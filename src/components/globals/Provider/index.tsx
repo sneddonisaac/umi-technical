470fb6aa59f04f246d82ac01bac9648c4ba52291
sneddonisaac <info.isaacsneddon@gmail.com>
2024-07-30 15:48:50 +0100

    fix: changing analytics back to original

--- src/components/globals/Provider/index.tsx
+++ src/components/globals/Provider/index.tsx
@@ -2,7 +2,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { inject } from '@vercel/analytics';
+import { Analytics } from "@vercel/analytics/react"
 
 // Screens
 import { Users, User, Error } from '../../screens';
@@ -25,11 +25,11 @@ const queryClient = new QueryClient();
 
 // A custom component that wraps the entire application with the QueryClientProvider and RouterProvider.
 export default function Provider() {
-  inject();
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
+      <Analytics />
     </QueryClientProvider>
   );
 }
