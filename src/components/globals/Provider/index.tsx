fe1a40b38c807333c567033314bce81e54b5d814
sneddonisaac <info.isaacsneddon@gmail.com>
2024-07-30 15:36:54 +0100

    upgrade: adding vercel analytics

--- src/components/globals/Provider/index.tsx
+++ src/components/globals/Provider/index.tsx
@@ -1,6 +1,10 @@
+// External
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Analytics } from "@vercel/analytics/react"
+
+// Screens
 import { Users, User, Error } from '../../screens';
 
 // Creating a new router instance.
@@ -25,6 +29,7 @@ export default function Provider() {
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
+      <Analytics />
     </QueryClientProvider>
   );
 }
